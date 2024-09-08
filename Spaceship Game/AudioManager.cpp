972a70d27cfe51a6e54488ca5a08938a168a20a3
leewei1702 <46920908+leewei1702@users.noreply.github.com>
2024-09-08 15:57:20 +0800

    Update AudioManager.cpp

--- Spaceship Game/AudioManager.cpp
+++ Spaceship Game/AudioManager.cpp
@@ -9,7 +9,7 @@ void AudioManager::InitializeAudio()
 void AudioManager::PlaySound1()
 {
 	result = system->playSound(sound1, 0, false, &channel);
-	channel->setVolume(1.5);
+	channel->setVolume(1);
 	//channel->setPan(0);
 	//channel->setPaused(false);
 }
@@ -40,12 +40,20 @@ void AudioManager::PlayHit()
 	//channel->setPaused(false);
 }
 
+void AudioManager::PlayBoom()
+{
+	result = system->playSound(sound5, 0, false, &channel5);
+	channel5->setVolume(2);
+	//channel->setPan(0);
+	//channel->setPaused(false);
+}
+
 
 void AudioManager::LoadSounds()
 {
 	
 	result = system->createSound("Assets/Sound/fallen-down.mp3", FMOD_DEFAULT, 0, &sound1);
-	result = sound1->setMode(FMOD_LOOP_OFF);
+	result = sound1->setMode(FMOD_LOOP_NORMAL);
 
 	result = system->createSound("Assets/Sound/shoot.wav", FMOD_DEFAULT, 0, &sound2);
 	result = sound2->setMode(FMOD_LOOP_OFF);
@@ -55,6 +63,9 @@ void AudioManager::LoadSounds()
 
 	result = system->createSound("Assets/Sound/hit.wav", FMOD_DEFAULT, 0, &sound4);
 	result = sound4->setMode(FMOD_LOOP_OFF);
+
+	result = system->createSound("Assets/Sound/boom.wav", FMOD_DEFAULT, 0, &sound5);
+	result = sound5->setMode(FMOD_LOOP_OFF);
 }
 
 void AudioManager::UpdateSound()
