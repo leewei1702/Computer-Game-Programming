c265128a1f0fee70968faf51e3a532c4dfc8c35d
leewei1702 <46920908+leewei1702@users.noreply.github.com>
2024-09-16 12:24:48 +0800

    Update AudioManager.h

--- Spaceship Game/AudioManager.h
+++ Spaceship Game/AudioManager.h
@@ -5,8 +5,8 @@ class AudioManager
 {
 public:
 	FMOD::System *system; //pointer to Virtual Sound card
-	FMOD::Sound *sound1, *sound2, *sound3, *sound4, *sound5, *sound6, *sound7; //sound files
-	FMOD::Channel *channel, *channel2, *channel3, *channel4, *channel5, *channel6, *channel7; //sound files are played and mixed
+	FMOD::Sound *sound1, *sound2, *sound3, *sound4, *sound5, *sound6, *sound7, *sound8; //sound files
+	FMOD::Channel *channel, *channel2, *channel3, *channel4, *channel5, *channel6, *channel7, *channel8; //sound files are played and mixed
 	FMOD_RESULT result;
 	void *extradriverdata = 0;
 
@@ -19,6 +19,7 @@ public:
 	void PlayBoom();
 	void PlayTheWorld();
 	void PlayPickUp();
+	void PlayButtonClick();
 	void LoadSounds(); //read sound file from Hdd, load to sound card
 	void UpdateSound(); //update any sound parameters - call EVERY loop
 	
